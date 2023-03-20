98774223ff3df76a1547da0da80d6e3dcf1be1e9
Kilo-5723 <yunxiang.zhao.stu@gmail.com>
2023-03-20 10:54:26 +0800

    update color

---
+++ format.py
@@ -0,0 +1,38 @@
+background = {'D': 'Aqua',
+              'C': 'Lime',
+              'B': 'SkyBlue',
+              'A': 'Orange',
+              'S': 'Red',
+              'T': 'Black',
+              'Y': 'HotPink',
+              'N': 'Silver'}
+
+text = {'D': 'black',
+        'C': 'black',
+        'B': 'black',
+        'A': 'black',
+        'S': 'black',
+        'T': 'white',
+        'Y': 'black',
+        'N': 'black'}
+
+
+def modify(s):
+    s = s[:-1]
+    if s[-3] == ',':
+        id = s[-1]
+        s, _ = s.split(',')
+        l, r = s.split('-', 1)
+        r.strip()
+        s = l + '- <span style = "background-color:' + background[id] + ';color:' + \
+            text[id]+'">&nbsp;' + r.strip() + '&nbsp;</span>'
+    return s
+
+
+f = open("knowledge-tree.md", "r")
+lines = f.readlines()
+for line in lines:
+    if len(line) > 3:
+        print(modify(line))
+    else:
+        print()
